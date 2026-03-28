189660a4513a8e9b2b6ba644c5d41d736af0eb4e
MaisamHalabi <maisam.halabi.1@outlook.com>
2026-03-28 18:10:20 +0300

    archetectured

--- analysis.py
+++ analysis.py
@@ -9,7 +9,6 @@ import numpy as np
 import matplotlib
 matplotlib.use("Agg")
 import matplotlib.pyplot as plt
-import numpy as np
 from scipy.stats import gaussian_kde
 from data.ring import RINGS_DICT
 
@@ -38,16 +37,7 @@ def load_all_csvs(csv_dir):
 
 
 # ---------------------- FILTERING ---------------------------
-def filter_rings(
-    df,
-    max_nodes,
-    ring_type=None,
-    node_type=None,
-    degree=None,
-    orientation_only=False,
-    num_rings=None,
-    with_benzene=False,
-):
+def filter_rings(df, max_nodes, ring_type=None, node_type=None, degree=None, orientation_only=False, num_rings=None, with_benzene=False):
 
     filtered = df.copy()
 
@@ -61,7 +51,7 @@ def filter_rings(
         # Now filter the already-filtered dataframe
         filtered = filtered[filtered["source_file"].isin(valid_molecules)]
 
-    if node_type is not None and not orientation_only:
+    if node_type is not None:
         filtered = filtered[filtered["type"] == node_type]
 
     if ring_type is not None:
@@ -73,128 +63,59 @@ def filter_rings(
         else:
             filtered = filtered[filtered["degree"] == degree]
     
+    if orientation_only:
+        filtered = filtered[filtered["node_idx"] >= max_nodes]
+
     if not with_benzene:
         filtered = filtered[filtered["type"] != "Bn"]
-
-        
 
     return filtered
 
 
 # ---------------------- PLOTTING ---------------------------
-def plot_iv_histogram(df, save_path, bins=50, label=None):
-    """
-    Plot smooth IV distribution using KDE with filled area.
-    """
-
-    iv_values = df["IV"].values
-    mean = np.mean(iv_values)
-    std = np.std(iv_values)
-
-    if len(iv_values) < 2:
-        print("Not enough data for KDE.")
-        return
-
-    kde = gaussian_kde(iv_values)
-
-    x_limit = max(np.abs(iv_values.min()), np.abs(iv_values.max()))
-    x = np.linspace((-1)*x_limit, x_limit, 500)
-    y = kde(x) * len(iv_values)
-
-    plt.figure(dpi=300)
-    final_label = f"{label} ({mean:.3f} ± {std:.3f})" if label else f"μ={mean:.3f} ± {std:.3f}"
-    plt.plot(x, y, color="blue", label=final_label)
-    plt.fill_between(x, y, color="blue", alpha=0.5)
-    plt.xlabel("IV value")
-    plt.ylim(bottom=0)
-    #plt.ylabel("Density")
-    plt.legend()
-    plt.tight_layout()
-    plt.savefig(save_path)
-    plt.close()
-
-
-def get_grouped_data(df, max_nodes, orientation_only, split_by_node_and_ring, split_by_ring_degree, node_type=None):
+def get_grouped_data(df, max_nodes, node_type=None, orientation_only=False, split_by_node_and_ring=False , split_by_ring_degree=False):
     """
     Returns an iterable of (label, sub_dataframe, node_type)
     depending on the selected grouping mode.
     """
 
-    # orientation atoms split by atom type + ring type + ring degree
-    if orientation_only and split_by_node_and_ring and split_by_ring_degree:
-        real_nodes = df[df["node_idx"] < max_nodes][["source_file", "node_idx", "degree"]].copy()
-        real_nodes = real_nodes.rename(columns={"node_idx": "Ring_indx", "degree": "ring_degree"})
-
-        df_orient = df[df["node_idx"] >= max_nodes].copy()
-
-        if node_type is not None:
-            df_orient = df_orient[df_orient["type"] == node_type]
-
-        df_orient = df_orient[df_orient["Ring_indx"].notna()].copy()
-
-        real_nodes["Ring_indx"] = real_nodes["Ring_indx"].astype(int)
-        df_orient["Ring_indx"] = df_orient["Ring_indx"].astype(int)
-
-        real_nodes["source_file"] = real_nodes["source_file"].astype(str)
-        df_orient["source_file"] = df_orient["source_file"].astype(str)
-
-        df_orient = df_orient.merge(real_nodes, on=["source_file", "Ring_indx"], how="left")
-
-        grouped = df_orient.groupby(["type", "Ring_type", "ring_degree"])
-        for (atom, ring, ring_degree), group in grouped:
-            yield f"{atom}-{ring}-deg{ring_degree}", group, atom
-            
-    # orientation atoms split by atom type + ring degree
-    elif orientation_only and split_by_ring_degree:
-        real_nodes = df[df["node_idx"] < max_nodes][["source_file", "node_idx", "degree"]].copy()
-        real_nodes = real_nodes.rename(columns={"node_idx": "Ring_indx", "degree": "ring_degree"})
-
-        df_orient = df[df["node_idx"] >= max_nodes].copy()
-
-        if node_type is not None:
-            df_orient = df_orient[df_orient["type"] == node_type]
-
-        df_orient = df_orient[df_orient["Ring_indx"].notna()].copy()
-
-        real_nodes["Ring_indx"] = real_nodes["Ring_indx"].astype(int)
-        df_orient["Ring_indx"] = df_orient["Ring_indx"].astype(int)
-
-        real_nodes["source_file"] = real_nodes["source_file"].astype(str)
-        df_orient["source_file"] = df_orient["source_file"].astype(str)
-
-        df_orient = df_orient.merge(real_nodes, on=["source_file", "Ring_indx"], how="left")
-
-        grouped = df_orient.groupby(["type", "ring_degree"])
-        for (atom, ring_degree), group in grouped:
-            yield f"{atom}-deg{ring_degree}", group, atom
-
-    # orientation atoms split by atom type + ring type
-    elif orientation_only and split_by_node_and_ring:
-        df_orient = df[df["node_idx"] >= max_nodes].copy()
-
-        if node_type is not None:
-            df_orient = df_orient[df_orient["type"] == node_type]
-
-        grouped = df_orient.groupby(["type", "Ring_type"])
-        for (atom, ring), group in grouped:
-            yield f"{atom}-{ring}", group, atom
-    # default separation
-    else:
-        if orientation_only:
-            df_orient = df[df["node_idx"] >= max_nodes].copy()
+    if orientation_only:
+        # orientation atoms split by atom type + ring type + ring degree
+        if split_by_node_and_ring and split_by_ring_degree:
+            grouped = df.groupby(["type", "Ring_type", "Ring_deg"])
+            for (atom, ring, ring_deg), group in grouped:
+                yield f"{atom}-{ring}-deg{ring_deg}", group, atom
+        
+        # orientation atoms split by atom type + ring degree
+        elif split_by_ring_degree:
+            grouped = df.groupby(["type", "Ring_deg"])
+            for (atom, ring_deg), group in grouped:
+                yield f"{atom}-deg{ring_deg}", group, atom
+        
+        # orientation atoms split by atom type + ring type
+        elif split_by_node_and_ring:
+            grouped = df.groupby(["type", "Ring_type"])
+            for (atom, ring), group in grouped:
+                yield f"{atom}-{ring}", group, atom
+    
+        # default separation - atoms
+        else:
+            df = df[df["node_idx"] >= max_nodes].copy()
             node_types = ATOMS_DICT
             for node_type in node_types:
-                group = df_orient[df_orient["type"] == node_type]
-                yield str(node_type), group, node_type
-        else:
-            df_real = df[df["node_idx"] < max_nodes].copy()
-            node_types = RINGS_DICT
-            for node_type in node_types:
-                group = df_real[df_real["type"] == node_type]
+                group = df[df["type"] == node_type]
                 yield str(node_type), group, node_type
 
+    # default separation - rings
+    else:
+        df = df[df["node_idx"] < max_nodes].copy()
+        node_types = RINGS_DICT
+        for node_type in node_types:
+            group = df[df["type"] == node_type]
+            yield str(node_type), group, node_type
 
-def plot_iv_histogram_by_ring_type(df, max_nodes, save_path, orientation_only, split_by_node_and_ring=False , split_by_ring_degree=False , node_type=None):
+
+def plot_iv_histogram_by_ring_type(df, max_nodes, save_path, node_type=None, orientation_only=False, split_by_node_and_ring=False , split_by_ring_degree=False):
 
     if "type" not in df.columns:
         print("Column 'type' not found.")
@@ -212,18 +133,10 @@ def plot_iv_histogram_by_ring_type(df, max_nodes, save_path, orientation_only, s
     x = np.linspace(-x_limit, x_limit, 500)
 
     plt.figure(dpi=300, figsize=(8, 6))
-
     plotted_any = False
 
     # unified grouping
-    for label_base, group, group_node_type in get_grouped_data(
-        df,
-        max_nodes,
-        orientation_only,
-        split_by_node_and_ring,
-        split_by_ring_degree,
-        node_type=node_type,
-    ):
+    for label_base, group, group_node_type in get_grouped_data(df, max_nodes, node_type, orientation_only, split_by_node_and_ring, split_by_ring_degree):
         iv_values = group["IV"].values
 
         if len(iv_values) < 2:
@@ -242,15 +155,13 @@ def plot_iv_histogram_by_ring_type(df, max_nodes, save_path, orientation_only, s
 
         label = f"{label_base} (n={len(iv_values)}, μ={mean:.3f} ± {std:.3f})"
         plt.plot(x, y, label=label)
+        plotted_any = True
     
     # Add aggregated node_type curves
-    if split_by_node_and_ring and orientation_only:
+    if orientation_only and (split_by_node_and_ring or split_by_ring_degree):
         print("Adding aggregated node_type curves...")
-        dict_to_use = ATOMS_DICT 
-        if node_type is not None:
-            dict_to_use = [node_type]
 
-        for node_type in dict_to_use:
+        for node_type in ATOMS_DICT:
             group = df[df["type"] == node_type]
             iv_values = group["IV"].values
 
@@ -270,18 +181,9 @@ def plot_iv_histogram_by_ring_type(df, max_nodes, save_path, orientation_only, s
 
             # distinguish visually
             label = f"{node_type} TOTAL (n={len(iv_values)}, μ={mean:.3f} ± {std:.3f})"
-
-            plt.plot(
-            x,
-            y,
-            linestyle="--",
-            linewidth=2,
-            label=label,
-            )
-
+            plt.plot(x, y, linestyle="--", linewidth=2, label=label)
             plotted_any = True
-            if node_type is not None:
-                plt.xlim(-0.15, 0.1)
+            
 
     if not plotted_any:
         print("No valid groups plotted.")
@@ -342,15 +244,17 @@ def analyze(
     if orientation_only:
         title_parts.append("orientation_only")
         if split_by_node_and_ring:
-            title_parts.append(f"sepereted")
+            title_parts.append(f"separatedByRingType")
+        if split_by_ring_degree:
+            title_parts.append(f"separatedByRingDeg")
     if node_type:
         title_parts.append(f"type={node_type}")
     if degree:
         title_parts.append(f"degree={degree}")
     if ring_type :
         title_parts.append(f"ring_type={ring_type}")
-    if split_by_ring_degree:
-        title_parts.append("split_by_ring_degree")
+    if with_benzene :
+        title_parts.append("withBn")
 
 
 
@@ -361,8 +265,7 @@ def analyze(
     #save_path = os.path.join(args.out_dir, "iv_histogram.png") #make sure path is right
 
     print("Plotting histogram...")
-    #plot_iv_histogram(df_filtered, save_path, bins=args.bins, label=label)
-    plot_iv_histogram_by_ring_type(df_filtered, max_nodes, save_path, orientation_only, split_by_node_and_ring, split_by_ring_degree , node_type)
+    plot_iv_histogram_by_ring_type(df_filtered, max_nodes, save_path, node_type, orientation_only, split_by_node_and_ring, split_by_ring_degree)
     print(f"Histogram saved to {save_path}")
 
 
@@ -407,7 +310,7 @@ def main(args):
             num_rings=cfg.get("num_rings"),
             split_by_node_and_ring=cfg.get("split_by_node_and_ring", False),
             split_by_ring_degree=cfg.get("split_by_ring_degree", False),
-            with_benzene=cfg.get("with_benzene", True),
+            with_benzene=cfg.get("with_benzene", False),
         )
 
 if __name__ == "__main__":
@@ -416,16 +319,7 @@ if __name__ == "__main__":
 
     parser.add_argument("--csv_dir", type=str, default="/home/maayanfarkash/proj/prediction_summary/hetro/hetro/analysis")
     parser.add_argument("--out_dir", type=str, default="analysis_output")
-
     parser.add_argument("--max_nodes", type=int, default=15)
-
-    #parser.add_argument("--ring_type", type=str, default=None)
-    #parser.add_argument("--node_type", type=str, default=None)
-    #parser.add_argument("--num_rings", type=int, default=None)
-    #parser.add_argument("--degree", type=int, nargs="+", default=None)
-    #parser.add_argument("--orientation_only", action="store_true")
-
-    parser.add_argument("--bins", type=int, default=50)
 
     args = parser.parse_args()
 
